baadd7255b7c9b0aff3b84be75300a679922075a
Leonardo <128467278+le0flo@users.noreply.github.com>
2025-06-21 16:42:47 +0200

    aggiornati i colori e il curriculum

--- src/CardsPanel/Curriculum/Curriculum.jsx
+++ src/CardsPanel/Curriculum/Curriculum.jsx
@@ -4,7 +4,7 @@ export function Curriculum() {
   return (
     <div className='card' id='curriculum'>
       <h1>Curriculum Vitae</h1>
-      <p>Ultima revisione: <b>22/5/2025</b></p>
+      <p>Ultima revisione: <b>21/6/2025</b></p>
       <a href='https://files.leoflo.me/lavoro/curriculum.pdf' className='button'>Mi assumi ?</a>
     </div>
   );
